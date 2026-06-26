f5fe5cc00760eb5f541e978251516104ca65daf1
JaysonEnriquePenaFerreras <202101805@p.uapa.edu.do>
2026-06-25 23:41:20 -0400

    Subir archivos backend

--- backend/config/env.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  dbFile: process.env.DB_FILE || "database/mibase.db",
-  corsOrigin: process.env.CORS_ORIGIN || "*"
-};
